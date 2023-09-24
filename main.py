6159b8dc0995a66b8f0e4fd4b6a7fbf8161e27ac
ArpinsFr <71224413+ArpinsFr@users.noreply.github.com>
2023-09-24 13:18:32 +0200

    tkt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from random import *
 from time import sleep
+import fonctions
 
 J11=""
 J12=""
@@ -20,9 +21,19 @@ J82=""
 J91=""
 J92=""
 
-ListeCartes = ['Ah','2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h', 'Jh', 'Qh', 'Kh', 'Ac','2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', '10c', 'Jc', 'Qc', 'Kc', 'Ad','2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', '10d', 'Jd', 'Qd', 'Kd', 'As','2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', '10s', 'Js', 'Qs', 'Ks',]
+M1=[]
+M2=[]
+M3=[]
+M4=[]
+M5=[]
+M6=[]
+M7=[]
+M8=[]
+M9=[]
+ 
+ListeCartes = ['Ah','2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', 'Th', 'Jh', 'Qh', 'Kh', 'Ac','2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', 'Tc', 'Jc', 'Qc', 'Kc', 'Ad','2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', 'Td', 'Jd', 'Qd', 'Kd', 'As','2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', 'Ts', 'Js', 'Qs', 'Ks',]
 DJoueurs = {"J1":[J11,J12],"J2":[J21,J22],"J3":[J31,J32],"J4":[J41,J42],"J5":[J51,J52],"J6":[J61,J62],"J7":[J71,J72],"J8":[J81,J82],"J9":[J91,J92]}
-
+Listemains=[M1,M2,M3,M4,M5,M6,M7,M8,M9]
 
 Cartes=ListeCartes.copy()
 Joueurs={}
@@ -36,6 +47,7 @@ while not nbjok:                         #tant qu'il est entre 2 et 9
     if nbj>1 and nbj <9:
         for i in range(nbj):
             Joueurs[listtemp[i]]=DJoueurs[listtemp[i]]
+        Listemains=Listemains[:nbj]
         nbjok=True
     elif nbj==9:
         nbjok=True
@@ -57,9 +69,23 @@ print()
 
 #Flop
 #while nbjs>1:
-F1 = Cartes.pop(randint(0,len(Cartes))) # Oh mais c'est gênant ! Quoicouflop
-F2 = Cartes.pop(randint(0,len(Cartes)))
-F3 = Cartes.pop(randint(0,len(Cartes)))
-F4 = Cartes.pop(randint(0,len(Cartes)))
-F5 = Cartes.pop(randint(0,len(Cartes)))
+F1 = Cartes.pop(randint(0,len(Cartes)-1)) # Oh mais c'est gênant ! Quoicouflop
+F2 = Cartes.pop(randint(0,len(Cartes)-1))
+F3 = Cartes.pop(randint(0,len(Cartes)-1))
+F4 = Cartes.pop(randint(0,len(Cartes)-1))
+F5 = Cartes.pop(randint(0,len(Cartes)-1))
 print(F1,F2,F3,F4,F5)
+
+templist=list(Joueurs.keys())
+for i in range(len(Listemains)):
+    #Listemains[i]=F1+F2+F3+F4+F5+Joueurs[templist[i][0]]+Joueurs[templist[i][1]]
+    Listemains[i].append(F1)
+    Listemains[i].append(F2)
+    Listemains[i].append(F3)
+    Listemains[i].append(F4)
+    Listemains[i].append(F5)
+    Listemains[i].append(Joueurs[templist[i]][0])
+    Listemains[i].append(Joueurs[templist[i]][1])
+    print(i+1,":",Listemains[i])
+    
+print(fonctions.quinte(M1))
