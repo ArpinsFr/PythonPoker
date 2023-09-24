01bd3d9e4cfd0611e0df500a682d13f5f9fed6b8
ninaz76 <145898074+ninaz76@users.noreply.github.com>
2023-09-24 10:47:06 +0200

    Update poker.py
    better list

--- poker.py
+++ poker.py
@@ -1,7 +1,7 @@
 from random import *
 import random
 
-Cartes = ['As de Pique', '2 de Pique','3 de Pique','4 de Pique','5 de Pique','6 de Pique', '7 de Pique','8 de Pique','9 de Pique','10 de Pique','Valet de Pique','Dame de Pique','Roi de Pique','As de Trefle', '2 de Trefle','3 de Trefle','4 de Trefle','5 de Trefle','6 de Trefle', '7 de Trefle','8 de Trefle','9 de Trefle','10 de Trefle','Valet de Trefle','Dame de Trefle','Roi de Trefle','As de Carreau', '2 de Carreau','3 de Carreau','4 de Carreau','5 de Carreau','6 de Carreau', '7 de Carreau','8 de Carreau','9 de Carreau','10 de Carreau','Valet de Carreau','Dame de Carreau','Roi de Carreau','As de Coeur', '2 de Coeur','3 de Coeur','4 de Coeur','5 de Coeur','6 de Coeur', '7 de Coeur','8 de Coeur','9 de Coeur','10 de Coeur','Valet de Coeur','Dame de Coeur','Roi de Coeur']
+Cartes = ['Ah','2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h', 'Jh', 'Qh', 'Kh', 'Ac','2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', '10c', 'Jc', 'Qc', 'Kc', 'Ad','2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', '10d', 'Jd', 'Qd', 'Kd', 'As','2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', '10s', 'Js', 'Qs', 'Ks',]
 J11 = randint(0,51) # 
 print(Cartes[J11])  #
 Cartes.pop(J11)     #
