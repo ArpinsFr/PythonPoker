3ccede5cd6f7cfeb90c9256eca025f2f393bf6ce
ninaz76 <145898074+ninaz76@users.noreply.github.com>
2023-09-24 10:49:24 +0200

    Update poker.py

--- poker.py
+++ poker.py
@@ -2,13 +2,13 @@ from random import *
 import random
 
 Cartes = ['Ah','2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', '10h', 'Jh', 'Qh', 'Kh', 'Ac','2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', '10c', 'Jc', 'Qc', 'Kc', 'Ad','2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', '10d', 'Jd', 'Qd', 'Kd', 'As','2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', '10s', 'Js', 'Qs', 'Ks',]
-J11 = randint(0,51) # 
-print(Cartes[J11])  #
+J11 = randint(0,51) # Joueur 1
+print(Cartes[J11])  # Carte 1
 Cartes.pop(J11)     #
 
-J12 = randint(0,50)
-print(Cartes[J12])
-Cartes.pop(J12)
+J12 = randint(0,50) # Joueur 1
+print(Cartes[J12])  # Carte 2
+Cartes.pop(J12)     #
 
-print(J11)
+print(J11)          # Vérifications
 print(J12)
