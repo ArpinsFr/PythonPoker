d45819633101395f26f5d24feeb71c78cf1d53f0
Arthur PINGARD <71224413+ArpinsFr@users.noreply.github.com>
2024-01-29 09:40:52 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -21,47 +21,48 @@ def tour(tour,nbjs):
 
 
 def suivre(mise,tmise,nbjs):
-  global F1,F2,F3,F4,F5,GBlinde,Banques,JoueursSuivent,Mises,Pot
-  BBanques=Banques.copy()
-  print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n","Cartes mileu :",flop)
-  sleep(2)
-  tmise=tour(tmise,nbjs)
-  for i in range(nbjs):
-      if i not in JTapis:
-        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-        print("Joueur",JoueursSuivent[tmise]+1,"\n")
-        print("Banque :",Banques[JoueursSuivent[tmise]])
-        print("Mise actuelle :",mise,"\n")
-        tok=False
-        if mise==0:
-          while not tok :
-            print("Check / Miser / Se Coucher")
-            a=input("C / M / F\n")
-            if a=="C":
-              tok=True
-            elif a=="M":
-              b=input("Mise ? (Minimum : "+str(GBlinde)+" )\n")
-              try:
-                b=int(b)
-              except:
-                pass
-              else: 
-                if b>=GBlinde and GBlinde<=Banques[JoueursSuivent[tmise]]:
-                  Banques[JoueursSuivent[tmise]]-=b
-                  Pot=Pot+(b-Mises[JoueursSuivent[tmise]])
-                  mise=b
-                  Mises[JoueursSuivent[tmise]]=b
-                  tok=True
-                  return(["R",tmise,mise])
-            elif a=="F":
-              nbjs-=1
-              JoueursSuivent[tmise]="Fold"
-              tok=True
-        if tmise==nbjs-1:
-          tmise=0
-        else:
-         tmise+=1
-  return[0]
+  if nbjs>1:
+    global F1,F2,F3,F4,F5,GBlinde,Banques,JoueursSuivent,Mises,Pot
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n","Cartes milieu :",flop)
+    sleep(2)
+    tmise=tour(tmise,nbjs)
+    for i in range(nbjs):
+        if i not in JTapis:
+          print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+          print("Joueur",JoueursSuivent[tmise]+1,"\n")
+          print("Banque :",Banques[JoueursSuivent[tmise]])
+          print("Mise actuelle :",mise,"\n")
+          tok=False
+          if mise==0:
+            while not tok :
+              print("Check / Miser / Se Coucher")
+              a=input("C / M / F\n")
+              if a=="C":
+                tok=True
+              elif a=="M":
+                b=input("Mise ? (Minimum : "+str(GBlinde)+" )\n")
+                try:
+                  b=int(b)
+                except:
+                  pass
+                else: 
+                  if b>=GBlinde and GBlinde<=Banques[JoueursSuivent[tmise]]:
+                    Banques[JoueursSuivent[tmise]]-=b
+                    Pot=Pot+(b-Mises[JoueursSuivent[tmise]])
+                    mise=b
+                    Mises[JoueursSuivent[tmise]]=b
+                    tok=True
+                    return(["R",tmise,mise])
+              elif a=="F":
+                JoueursSuivent[tmise]="Fold"
+                tok=True
+          if tmise==nbjs-1:
+            tmise=0
+          else:
+           tmise+=1
+    return[0]
+  else:
+    suiviok=True
 def suivre2(mise,tmise,nbjs):
     global F1,F2,F3,F4,F5,GBlinde,Banques,JoueursSuivent,Mises,Pot
     tmise=tour(tmise,nbjs)
@@ -95,7 +96,6 @@ def suivre2(mise,tmise,nbjs):
                 tok=True
                 return(["R",tmise,mise])
           elif a=="F":
-            nbjs-=1
             JoueursSuivent[tmise]="Fold"
             tok=True
         if tmise==nbjs-1:
@@ -103,14 +103,17 @@ def suivre2(mise,tmise,nbjs):
         else:
           tmise+=1
     return[0] 
-def fold():
-  global JoueursSuivent
-  for i in range(len(JoueursSuivent)):
-    if JoueursSuivent[i] == "Fold":
-      JoueursSuivent.pop(i)
   
-
-
+def fold():
+  global JoueursSuivent,nbjs
+  i=0
+  while i<nbjs:
+    if JoueursSuivent[i] == "Fold":
+      nbjs-=1
+      JoueursSuivent.pop(i)
+    else:
+      i+=1
+    
 DeJoueurs = {"J1":[0,0],"J2":[0,0],"J3":[0,0],"J4":[0,0],"J5":[0,0],"J6":[0,0],"J7":[0,0],"J8":[0,0],"J9":[0,0]}
 Joueurs2={}
 Listemains2=[[],[],[],[],[],[],[],[],[]]
@@ -193,14 +196,10 @@ while nbjv>1 :
     Mises[i]=0
   input("Appuyez pour commencer le tour")
   
-  
-  #while nbjv > 1:  --->  Lui il est bien placé on s'en servira que à la fin pour faire les boucles
-  
   nbjs = nbjv                       # Initialise le nombre de joueur qui suivent la main
-  
   if Tour == nbjv-1:
     Banques[Tour]-=PBlinde
-    Mises[Tour]=PBlibde
+    Mises[Tour]=PBlinde
     Banques[0]-=GBlinde
     Mises[0]=GBlinde
   else:
@@ -208,121 +207,142 @@ while nbjv>1 :
     Mises[Tour]=PBlinde
     Banques[Tour+1]-=GBlinde
     Mises[Tour+1]=GBlinde
-  Pot=PBlinde+GBlinde
-
-# Blindes
-  
+  Pot+=PBlinde+GBlinde
+  # Blindes
+    
   for i in Joueurs.keys():
       Joueurs[i][0] = Cartes.pop(randint(0,len(Cartes)-1)) # Joueur 1 Carte 1
       Joueurs[i][1] = Cartes.pop(randint(0,len(Cartes)-1)) # Joueur 2 Carte 2
       print(i,Joueurs[i][0],Joueurs[i][1])
-
+  
   suivi=False
+  
   templist=list(Joueurs.keys())
   for i in range(len(Listemains)):
     Listemains[i].append(Joueurs[templist[i]][0])
     Listemains[i].append(Joueurs[templist[i]][1])
-  #Flop
-     
-  F1 = Cartes.pop(randint(0,len(Cartes)-1)) # Oh mais c'est gênant ! Quoicouflop
+
+  suivi=suivre(0,Tour,nbjs)
+  suiviok=False
+  while not suiviok:  
+      fold()
+      if suivi[0]=="R":
+        suivi=suivre2(suivi[2],suivi[1]-1,nbjs)
+      else:
+        suiviok=True
+    
+    #Flop
+       
+  F1 = Cartes.pop(randint(0,len(Cartes)-1))
   F2 = Cartes.pop(randint(0,len(Cartes)-1))
   F3 = Cartes.pop(randint(0,len(Cartes)-1))
   flop.append(F1)
   flop.append(F2)
   flop.append(F3)
-  templist=list(Joueurs.keys())                   #Donne pour chaque joueur sa main totale ( 7 cartes )
+  templist=list(Joueurs.keys())    #Donne pour chaque joueur sa main totale ( 7 cartes )
   for i in range(len(Listemains)):
-      Listemains[i].append(F1)
-      Listemains[i].append(F2)
-      Listemains[i].append(F3)
-    
+        Listemains[i].append(F1)
+        Listemains[i].append(F2)
+        Listemains[i].append(F3)
+  
+  for i in range(nbjs):
+      Mises[JoueursSuivent[i]]=0
+  if nbjs>1:
+      suiviok=False
   suivi=suivre(0,Tour,nbjs)
-  suiviok=False
   while not suiviok:  
-    fold()
-    if suivi[0]=="R":
-      suivi=suivre2(suivi[2],suivi[1]-1,nbjs)
-    else:
-      suiviok=True
+      fold()
+      if suivi[0]=="R":
+        suivi=suivre2(suivi[2],suivi[1]-1,nbjs)
+      else:
+        suiviok=True
+  
+  #River 
     
-
-  #On est censé mettre un truc ici pour les mises
-  suiviok=False
+  if nbjs>1:
+      suiviok=False
   F4 = Cartes.pop(randint(0,len(Cartes)-1))
   flop.append(F4)
   for i in range(len(Listemains)):
-    Listemains[i].append(F4)
+      Listemains[i].append(F4)
+  for i in range(nbjs):
+      Mises[JoueursSuivent[i]]=0
   suivi=suivre(0,Tour,nbjs)
+  print(suiviok) #débug
   while not suiviok:  
-    fold()
-    if suivi[0]=="R":
-      suivi=suivre2(suivi[2],suivi[1]-1,nbjs)
-    else:
-      suiviok=True
+      fold()
+      if suivi[0]=="R":
+        suivi=suivre2(suivi[2],suivi[1]-1,nbjs)
+      else:
+        suiviok=True
   
-  # Ici aussi
-  suiviok=False
+    #Turn
+    
+  if nbjs>1:
+    suiviok=False
   F5 = Cartes.pop(randint(0,len(Cartes)-1))
   flop.append(F5)
   for i in range(len(Listemains)):
-    Listemains[i].append(F5)
+      Listemains[i].append(F5)
+  for i in range(nbjs):
+      Mises[JoueursSuivent[i]]=0
   suivi=suivre(0,Tour,nbjs)
   while not suiviok:  
-    fold()
-    if suivi[0]=="R":
-      suivi=suivre2(suivi[2],suivi[1]-1,nbjs)
-    else:
-      suiviok=True
-
+      fold()
+      if suivi[0]=="R":
+        suivi=suivre2(suivi[2],suivi[1]-1,nbjs)
+      else:
+        suiviok=True
+  
   print(F1,F2,F3,F4,F5)
 
-  for i in range (nbj):                           # DEBUG --- Affiche les mains totales de chaque joueurs + les combinaisons 
+  for i in range (nbjs):    # DEBUG --- Affiche les mains totales de chaque joueurs + les combinaisons 
     print("\n","JOUEUR",i+1,"\n")
     print(Listemains[i])
-    Checkmain[i][0]=fonctions.check(Listemains[i]).copy()
-    print(fonctions.check(Listemains[i]))
-    Checkmain[i][1]=fonctions.quinte(Listemains[i])
-    print("Quinte :",fonctions.quinte(Listemains[i]))
-    Checkmain[i][2]=fonctions.flush(Listemains[i]).copy()
-    print("Flush :",fonctions.flush(Listemains[i]))
+    Checkmain[JoueursSuivent[i]][0]=fonctions.check(Listemains[JoueursSuivent[i]]).copy()
+    print(fonctions.check(Listemains[JoueursSuivent[i]]))
+    Checkmain[JoueursSuivent[i]][1]=fonctions.quinte(Listemains[JoueursSuivent[i]])
+    print("Quinte :",fonctions.quinte(Listemains[JoueursSuivent[i]]))
+    Checkmain[JoueursSuivent[i]][2]=fonctions.flush(Listemains[JoueursSuivent[i]]).copy()
+    print("Flush :",fonctions.flush(Listemains[JoueursSuivent[i]]))
 
   print()
   print()
   
-  for i in range(nbj):
-    if Checkmain[i][2][0]=='QF':
-      Listemains[i]=Checkmain[i][2]
-      Points[i]=8
-    elif Checkmain[i][0][0]=='Carre' or Checkmain[i][0][0]=='Full':
-      Listemains[i]=Checkmain[i][0]
-      if Checkmain[i][0][0]=='Carre':
-        Points[i]=7
+  for i in range(nbjs):
+    if Checkmain[JoueursSuivent[i]][2][0]=='QF':
+      Listemains[JoueursSuivent[i]]=Checkmain[JoueursSuivent[i]][2]
+      Points[JoueursSuivent[i]]=8
+    elif Checkmain[JoueursSuivent[i]][0][0]=='Carre' or Checkmain[JoueursSuivent[i]][0][0]=='Full':
+      Listemains[JoueursSuivent[i]]=Checkmain[JoueursSuivent[i]][0]
+      if Checkmain[JoueursSuivent[i]][0][0]=='Carre':
+        Points[JoueursSuivent[i]]=7
       else:
-        Points[i]=6
-    elif Checkmain[i][2][0]=='F':
-      Listemains[i]=Checkmain[i][2]
-      Points[i]=5
-    elif Checkmain[i][1][0]=='Q':
-      Listemains[i]=Checkmain[i][1]
-      Points[i]=4
+        Points[JoueursSuivent[i]]=6
+    elif Checkmain[JoueursSuivent[i]][2][0]=='F':
+      Listemains[JoueursSuivent[i]]=Checkmain[JoueursSuivent[i]][2]
+      Points[JoueursSuivent[i]]=5
+    elif Checkmain[JoueursSuivent[i]][1][0]=='Q':
+      Listemains[JoueursSuivent[i]]=Checkmain[JoueursSuivent[i]][1]
+      Points[JoueursSuivent[i]]=4
     else:
-      Listemains[i]=Checkmain[i][0]
-      if Checkmain[i][0][0]=='Brelan':
-        Points[i]=3
-      elif Checkmain[i][0][0]=='DPaire':
-        Points[i]=2
-      elif Checkmain[i][0][0]=='Paire':
-        Points[i]=1
-    print(Listemains[i])
+      Listemains[JoueursSuivent[i]]=Checkmain[JoueursSuivent[i]][0]
+      if Checkmain[JoueursSuivent[i]][0][0]=='Brelan':
+        Points[JoueursSuivent[i]]=3
+      elif Checkmain[JoueursSuivent[i]][0][0]=='DPaire':
+        Points[JoueursSuivent[i]]=2
+      elif Checkmain[JoueursSuivent[i]][0][0]=='Paire':
+        Points[JoueursSuivent[i]]=1
+    print(Listemains[JoueursSuivent[i]])
 
   indl=[]
   m=Points[0]
-  for i in range(nbj):
-    if Points[i]>m:
-      m=Points[i]
-      indl=[i]
-    elif Points[i]==m:
-      indl.append(i)
+  for i in range(nbjs):
+    if Points[JoueursSuivent[i]]>m:
+      m=Points[JoueursSuivent[i]]
+      indl=[JoueursSuivent[i]]
+    elif Points[JoueursSuivent[i]]==m:
+      indl.append(JoueursSuivent[i])
   print()
   print(indl)
 
@@ -390,6 +410,15 @@ while nbjv>1 :
     print(egal)
     
 
+  i=0
+  while i<nbjs:
+    if Banques[Listejoueurs[i]] <= 0:
+      nbjv-=1
+      Listejoueurs.pop(i)
+    else:
+      i+=1
+
+  
   Tour+=1
   if Tour == nbjv:
     Tour = 0
